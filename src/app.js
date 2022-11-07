96269319d4cf98e2d84c7aa9c3ff4662e2d434ee
Woodsey <timjameswoods@gmail.com>
2022-11-07 20:30:58 +0000

    Another update

--- src/app.js
+++ src/app.js
@@ -12,11 +12,15 @@ app.get('/help', (req, res) => {
 });
 
 app.get('/about', (req, res) => {
-    res.send('About Page')
+    res.send('<h1>About Page</h1>')
 });
 
 app.get('/weather', (req, res) => {
-    res.send('Weather Page')
+    const data = {
+        forecast: '',
+        location: ''
+    }
+    res.send(data);
 });
 
 app.listen(3000, () => {
