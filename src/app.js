3709da64a08134dfd683f15516329171e4ec6d05
Woodsey <timjameswoods@gmail.com>
2022-11-08 21:31:55 +0000

    styles update

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
 
 
 console.log(__dirname);
@@ -7,24 +8,39 @@ console.log(path.join(__dirname, '../public'));
 
 
 const app = express();
+
+// define paths for express config
 const publicPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// set up handlebars and view location
 app.set('view engine', 'hbs'); // sets up handlebars with express
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
 app.use(express.static(publicPath));
 
 
 app.get('', (req, res) => {
-    res.render('index')
+    res.render('index', {
+        title: 'Something different'
+    })
 });
 
-/*
 app.get('/help', (req, res) => {
-    res.redirect('/help.html');
+    res.render('help', {
+        title: 'Help Page',
+        message: 'First, seek to understand'
+    })
 });
 
 app.get('/about', (req, res) => {
-    res.redirect('/about.html');
+    res.render('about', {
+        title: 'About Page'
+    })
 });
-*/
+
 
 app.get('/weather', (req, res) => {
     const data = {
@@ -32,6 +48,19 @@ app.get('/weather', (req, res) => {
         location: ''
     }
     res.send(data);
+});
+
+
+app.get('/help/*', (req, res) => {
+    res.render('errors', {
+        errormessage: 'Missing help page - more help on the way soon!'
+    });
+});
+
+app.get('*', (req, res) => {
+    res.render('errors', {
+        errormessage: 'Generic error 404 page'
+    });
 });
 
 app.listen(3000, () => {
