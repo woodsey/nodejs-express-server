a74845f7f2a84dd04749f16495fb2fe16684e067
Woodsey <timjameswoods@gmail.com>
2022-11-09 20:34:06 +0000

    working on the form

--- public/js/app.js
+++ public/js/app.js
@@ -2,21 +2,34 @@ console.log('Client side javascript file loaded');
 
 
 //{ mode: "no-cors" }
+// this code is not working, something wrong with the response but cannot figure out why
+/*
 fetch('http://puzzle.mead.io/puzzle').then((response) => {
     console.log(response)
     response.json().then((data) => {
         console.log(data)
     })
 });
+*/
 
-
-fetch('http://localhost:3000/weather?address=Southampton').then((response) => {
-    response.json().then((data) => {
-        if (data.error) {
-            console.log(data.error)
-        } else {
-            console.log(data.location)
-            console.log(data.forecast)
-        }
+if (false) {
+    fetch('http://localhost:3000/weather?address=Southampton').then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                console.log(data.error)
+            } else {
+                console.log(data.location)
+                console.log(data.forecast)
+            }
+        })
     })
-})
+}
+
+
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const location = search.value;
+    console.log('value: ' + location)
+});
